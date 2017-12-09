4e21232736ae40424a582e4d1928f79b07f2c432
SJFY <jiefeis2@illinois.edu>
2017-12-09 15:56:12 -0600

    done

--- csma.cpp
+++ csma.cpp
@@ -25,6 +25,10 @@ long int total_collisions = 0;
 long int channel_utilization = 0;
 //Total channel idle time
 long int channel_idle = 0;
+//Transmission variance among all nodes
+double trans_var = 0;
+//Collision variance among all nodes
+double colli_var = 0;
 
 void readFile(string fname) {
 	ifstream infile(fname.c_str());
@@ -124,24 +128,24 @@ int getNewR_backoff(int currentR) {
 void simulation() {
 	srand (time(NULL));
 	std::vector<int> collisionNode_index;
-	for (long long int i = 0; i < 100; i ++) {
+	for (long long int i = 0; i < T; i ++) {
 		for (int i = 0; i < N; i ++) {
 			if (nodes[i]->backoff == 0) {
 				collisionNode_index.push_back(i);
 			}
 		}
-		printf("the size is %d\n", collisionNode_index.size());
+		// printf("the size is %d\n", collisionNode_index.size());
 	//	printf("the first index is %d\n",collisionNode_index[0]);
 		int collisionNode_num = collisionNode_index.size();
 		// int collisionNode_num = 0;
 		if (collisionNode_num == 0) {
 			//No collision!
 			//Just decrement every node's backoff value by 1
-			//The channel is idle, increase the channel_idel by 1
+			//The channel is idle, increase the channel_idle by 1
 			allNodes_decre_backoff();
 			channel_idle ++;
-			printf("all nodes backoff\n");
-			test();
+			// printf("all nodes backoff\n");
+			// // test();
 		}
 		else if (collisionNode_num == 1) {
 			//Start transmit!
@@ -150,6 +154,12 @@ void simulation() {
 			//Increase i += L - 1, for the entire transmission time
 			//Reset the currentR, backoff and retrans_num to 0
 			//Increase the transmission_num by 1
+			if (L > T - i) {
+				channel_utilization += T - i;
+				i += T - i;
+				break;
+			}
+			else {
 			i += L - 1;
 			
 			int index = collisionNode_index[0];
@@ -158,14 +168,15 @@ void simulation() {
 			nodes[index]->backoff = rand()%(R[0] + 1);
 			nodes[index]->retrans_num = 0;
 			channel_utilization += L;
-			printf("bingo! no collision, will start transmission\n");
-			printf("index is %d\n", index );
-			printf("new transmission_num is %d and backoff is %d\n", nodes[index]->transmission_num, nodes[index]->backoff );
-			printf("the new i is %d \n",i );
-			test();
+			// printf("bingo! no collision, will start transmission\n");
+			// printf("index is %d\n", index );
+			// printf("new transmission_num is %d and backoff is %d\n", nodes[index]->transmission_num, nodes[index]->backoff );
+			// printf("the new i is %d \n",i );
+			}
+			// test();
 		}
 		else {
-			printf("collision happens\n");
+			// printf("collision happens\n");
 			//Collision happens!
 			//For all the collision nodes, double their currentR, increase retrans_num by 1, recalculate backoff value
 			//For all the collision nodes, increase thier collision_num by 1
@@ -188,9 +199,279 @@ void simulation() {
 				nodes[index]->collision_num ++;
 			}
 			total_collisions ++;
-			test();
+			//Because the unused clock ticks do not include collisions
+			// channel_idle ++;
+			// test();
 		}
 		collisionNode_index.clear();
+	}
+	printf("simulation finished\n");
+	printf("total_collisions is %ld\n", total_collisions);
+	printf("channel_idle time is %ld\n", channel_idle);
+	printf("channel_utilization time is %ld\n", channel_utilization );
+}
+
+void printOutput() {
+	double trans_mean, colli_mean;
+	double trans_sum, colli_sum;
+	std::vector<int> trans;
+	std::vector<int> colli;
+	for (int i = 0; i < N; i ++) {
+		trans.push_back(nodes[i]->transmission_num);
+		colli.push_back(nodes[i]->collision_num);
+		trans_sum += trans[i];
+		colli_sum += colli[i];
+		printf("node %d collision_num is %d transmission_num is %d\n", i, nodes[i]->collision_num, nodes[i]->transmission_num);
+	}
+	trans_mean = trans_sum/ (double) N;
+	colli_mean = colli_sum/ (double) N;
+	double tmp1 = 0;
+	double tmp2 = 0;
+	for (int i = 0; i < N; i ++) {
+		tmp1 += (trans_mean - (double) trans[i]) * (trans_mean - (double) trans[i]);
+		tmp2 += (colli_mean - (double) colli[i]) * (colli_mean - (double) colli[i]);
+	}
+	trans_var = tmp1 / (double) N;
+	colli_var = tmp2 / (double) N;
+	printf("trans_mean is %f\n", trans_mean );
+	printf("colli_mean is %f\n", colli_mean );
+	printf("trans_var is %f\n", trans_var );
+	printf("colli_var is %f\n", colli_var);
+
+	ofstream outfile;
+	outfile.open("output.txt");
+	outfile << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	outfile << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	outfile << "Total number of collisions: " << total_collisions << "\n";
+	outfile << "Variance in number of successful transmissions: " << trans_var << "\n";
+	outfile << "Variance in number of collisions: " << colli_var << "\n";
+	outfile.close();
+}
+
+void printGraph() {
+	ofstream f1("plota.txt");
+	ofstream f2("plotb.txt");
+	ofstream f3("plotc.txt");
+	ofstream f41("plotd1.txt");
+	ofstream f42("plotd2.txt");
+	ofstream f43("plotd3.txt");
+	ofstream f44("plotd4.txt");
+	ofstream f45("plotd5.txt");
+	ofstream f51("plote1.txt");
+	ofstream f52("plote2.txt");
+	ofstream f53("plote3.txt");
+	ofstream f54("plote4.txt");
+	ofstream f55("plote5.txt");
+
+	//Print f1, f2, f3
+	for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 20;
+		R.push_back(1);
+		R.push_back(2);
+		R.push_back(4);
+		R.push_back(8);
+		R.push_back(16);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f41 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+	}
+
+	for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 20;
+		R.push_back(2);
+		R.push_back(4);
+		R.push_back(8);
+		R.push_back(16);
+		R.push_back(32);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f42 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+	}
+	for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 20;
+		R.push_back(4);
+		R.push_back(8);
+		R.push_back(16);
+		R.push_back(32);
+		R.push_back(64);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f43 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+
+	}
+	for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 20;
+		R.push_back(8);
+		R.push_back(16);
+		R.push_back(32);
+		R.push_back(64);
+		R.push_back(128);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f44 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+	}
+	for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 20;
+		R.push_back(16);
+		R.push_back(32);
+		R.push_back(64);
+		R.push_back(128);
+		R.push_back(256);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f45 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+	}
+	for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 20;
+		R.push_back(8);
+		R.push_back(16);
+		R.push_back(32);
+		R.push_back(64);
+		R.push_back(128);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f51 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+	}
+	for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 40;
+		R.push_back(8);
+		R.push_back(16);
+		R.push_back(32);
+		R.push_back(64);
+		R.push_back(128);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f52 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+	}for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 60;
+		R.push_back(8);
+		R.push_back(16);
+		R.push_back(32);
+		R.push_back(64);
+		R.push_back(128);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f53 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+	}for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 80;
+		R.push_back(8);
+		R.push_back(16);
+		R.push_back(32);
+		R.push_back(64);
+		R.push_back(128);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f54 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
+	}for (int i = 5; i <= 100; i++) {
+		R.clear();
+		nodes.clear();
+		N = i;
+		L = 100;
+		R.push_back(8);
+		R.push_back(16);
+		R.push_back(32);
+		R.push_back(64);
+		R.push_back(128);
+		M = 6;
+		T = 50000;
+		channel_utilization = 0;
+		channel_idle = 0;
+		total_collisions = 0;
+		createNodes();
+		simulation();
+		f55 << "Channel utilization: " << ((double) channel_utilization / (double) T) * 100.0 << "%\n";
+	// 	f2 << "Channel idle fraction: " << ((double) channel_idle / (double) T) * 100.0 << "%\n";
+	// 	f3 << "Total number of collisions: " << total_collisions << "\n";
 	}
 }
 int main(int argc, char** argv){
@@ -206,8 +487,6 @@ int main(int argc, char** argv){
 	printf("Channel Used: %d\n", L);
 	createNodes();
 	simulation();
-	// create_nodes();
-	// runsimulation();
-	// getResults();
-	// printGraphData();
+	printOutput();
+	printGraph();
 }
